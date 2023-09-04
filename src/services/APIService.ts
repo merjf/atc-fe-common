6253e7dade70a44d11428495018b79a62cb9e175
Francesco Mergiotti <merjf@Francescos-MacBook-Air.local>
2023-09-04 16:22:08 +0200

    Updated URLs

--- src/services/APIService.ts
+++ src/services/APIService.ts
@@ -1,19 +1,20 @@
 import { Image } from "../models/types";
 
-const GATEWAY_URL = "http://127.0.0.1:5002/atc/"
+const GATEWAY_URL = "http://127.0.0.1:5001"
+const OBJECT_DETECTION_SERVICE_PREFIX = "/atc-be-object-detection"
+const MONTECARO_SERVICE_PREFIX = "/atc-be-montecarlo"
 
-const OBJECT_DETECTION_LOAD_DATASET = GATEWAY_URL + "load-dataset"
-const OBJECT_DETECTION_TEST_MODEL = GATEWAY_URL + "test-object-model"
+const OBJECT_DETECTION_LOAD_DATASET = GATEWAY_URL + OBJECT_DETECTION_SERVICE_PREFIX + "/load-dataset"
+const OBJECT_DETECTION_TEST_MODEL = GATEWAY_URL + OBJECT_DETECTION_SERVICE_PREFIX + "/test-object-model"
+
+const MONTECARLO_GET_DECK = GATEWAY_URL + MONTECARO_SERVICE_PREFIX + "/get-deck"
 
 const requestFileOptions = (body: any) => ({
     method: 'POST',
     body: body
 });
 
-// export const fetchObjectDatasetInfo = () => {
-//   return fetch(OBJECT_DETECTION_RESULT_INFO)
-//     .then(response => response.json())
-// }
+// Object Detection Service APIs
 
 export const fetchObjectDetectionLoadDataset = (dataset:string) => {
   return fetch(OBJECT_DETECTION_LOAD_DATASET+"?dataset="+dataset)
@@ -24,5 +25,12 @@ export const fetchObjectModelTesting = (image : Image) => {
   const body = new FormData();
   body.append("image", image.value);
   return fetch(OBJECT_DETECTION_TEST_MODEL, requestFileOptions(body))
+    .then(response => response.json())
+}
+
+// Montecarlo Simulation Service APIs
+
+export const fetchDeck = () => {
+  return fetch(MONTECARLO_GET_DECK)
     .then(response => response.json())
 }
